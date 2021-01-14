c9b26ba0b5da244938315694b7f183580c525aa2
expdani <daniverhaar@hotmail.com>
2021-01-14 16:35:52 +0100

    Final version

--- src/listeners/stockListener.ts
+++ src/listeners/stockListener.ts
@@ -1,14 +1,18 @@
 import {client} from "../";
-const stockFinder = require('stockfinder');
-const reloadInterval = 5;
-const sf = new stockFinder('stable', 'NIO', 'pk_69657184759b494f835eb91a4c20e072', false);
+import {env} from "../../environment";
 
+const stockFinder = require('stockfinder');
+// const CurrencyConverter = require('@y2nk4/currency-converter')
+const reloadInterval = 5;
+const sf = new stockFinder('stable', 'NIO', env.IEX_API_KEY, false);
+
+// const converter = new CurrencyConverter('do-not-use-this-key')
 
 /**
  * Listener that listens to messages send in a server
  */
 export default function setupStockListeners() {
-    setInterval(()=> { update() }, reloadInterval * 1000);
+    setInterval(() => { update() }, reloadInterval * 1000);
 }
 
 function update() {
@@ -19,7 +23,10 @@ function update() {
     sf.getStock().then((res: any) => {
         client.guilds.forEach(guild => {
             const response = JSON.parse(JSON.stringify(res));
-            guild.me.setNickname(response[0].lastSalePrice.toString());
+            guild.me.setNickname(response[0].lastSalePrice.toString() + " USD");
+            // converter.convert('USD', 'EUR', response[0].lastSalePrice).then((value: any) => {
+            //     guild.me.setNickname(value.toString() + " EUR");
+            // });
         });
     });
 }
